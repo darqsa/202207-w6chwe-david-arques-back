c056b96b9fcfb78179a2fe38f228a9f57ca2f6a3
Carlos <casantosmu@gmail.com>
2022-08-13 00:02:50 +0200

    Added test exclusions

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,9 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   passWithNoTests: true,
+  collectCoverageFrom: [
+    "!src/index.ts",
+    "!src/server/startServer.ts",
+    "!src/database/index.ts",
+  ],
 };
