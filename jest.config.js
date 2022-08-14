4381905f5916b43b6f21fdb16486c103655dba45
Carlos <casantosmu@gmail.com>
2022-08-14 15:12:33 +0200

    Modified testcogerage to exclude routers and dist

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,10 @@ module.exports = {
   testEnvironment: "node",
   passWithNoTests: true,
   collectCoverageFrom: [
+    "src/**/*.ts",
     "!src/index.ts",
     "!src/server/startServer.ts",
     "!src/database/index.ts",
+    "!src/server/routers/robotsRouter.ts",
   ],
 };
