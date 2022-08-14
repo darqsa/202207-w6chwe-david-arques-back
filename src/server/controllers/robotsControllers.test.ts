aa889420cd86f13645826fb1a12888a20238f24c
Carlos <casantosmu@gmail.com>
2022-08-14 19:43:33 +0200

    Modified getRobots test

--- src/server/controllers/robotsControllers.test.ts
+++ src/server/controllers/robotsControllers.test.ts
@@ -2,11 +2,11 @@ import { Request, Response } from "express";
 import Robot from "../../database/models/Robot";
 import getRobots from "./robotsControllers";
 
-describe("Given a getRobots controller", () => {
-  afterEach(() => {
-    jest.resetModules();
-  });
+jest.mock("../../utils/parseRobotDB", () =>
+  jest.fn().mockReturnValueOnce("WallE").mockReturnValueOnce("Laika")
+);
 
+describe("Given a getRobots controller", () => {
   describe("When it recives a response", () => {
     const req: Partial<Request> = {};
     const res: Partial<Response> = {
@@ -23,16 +23,16 @@ describe("Given a getRobots controller", () => {
       expect(res.status).toHaveBeenCalledWith(expectedStatus);
     });
 
-    // describe("And Robots.find() returns a list with WallE and Laika", () => {
-    //   test("Then it should call the json method with the list with WallE and Laika", async () => {
-    //     const robots: string[] = ["WallE", "Laika"];
+    describe("And Robots.find() and parseRobotDB() returns a list with WallE and Laika", () => {
+      test("Then it should call the json method with the list with WallE and Laika", async () => {
+        const robots = ["WallE", "Laika"];
 
-    //     Robot.find = jest.fn().mockResolvedValue(robots);
+        Robot.find = jest.fn().mockResolvedValue(robots);
 
-    //     await getRobots(req as Request, res as Response);
+        await getRobots(req as Request, res as Response);
 
-    //     expect(res.json).toHaveBeenCalledWith({ robots });
-    //   });
-    // });
+        expect(res.json).toHaveBeenCalledWith({ robots });
+      });
+    });
   });
 });
