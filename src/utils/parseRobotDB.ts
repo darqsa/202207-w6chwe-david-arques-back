34b4d294e5e91150f3115c1831f1fa479fb8c385
Carlos <casantosmu@gmail.com>
2022-08-14 15:30:49 +0200

    Added name propierty to parsed robot

--- src/utils/parseRobotDB.ts
+++ src/utils/parseRobotDB.ts
@@ -2,10 +2,12 @@ import { RobotDBResponse } from "../types/interfaces";
 
 const parseRobotDB = ({
   _id,
+  name,
   image,
   stats: { strength, speed, creationDate },
 }: RobotDBResponse) => ({
   id: _id.toString(),
+  name,
   image,
   stats: {
     strength,
