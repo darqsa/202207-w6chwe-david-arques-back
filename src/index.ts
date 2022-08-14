47e8c2906783f7fb119aacb35ea85fd3523c4236
Carlos <casantosmu@gmail.com>
2022-08-14 15:13:15 +0200

    Added CORS dependency && Added cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import "./loadEnvironment";
+import cors from "cors";
 import { app, startServer } from "./server/startServer";
 import connectDB from "./database";
 import robotsRouter from "./server/routers/robotsRouter";
 import { generalError, notFoundError } from "./server/middlewares/errors";
 
-const port = process.env.PORT ?? 6666;
+const port = process.env.PORT ?? 4444;
 const mongoUrl = process.env.DDBB;
+
+app.use(cors());
 
 app.use("/robots", robotsRouter);
 
