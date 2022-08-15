9c1bd0ce42b4c2383a0fbe9066ce9e12ac8ef461
Carlos <casantosmu@gmail.com>
2022-08-15 18:49:48 +0200

    Added morgan dependency && included morgan middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import robotsRouter from "./routers/robotsRouter";
 import { generalError, notFoundError } from "./middlewares/errors";
 
 const app = express();
 
 app.use(cors());
+app.use(morgan("dev"));
 
 app.use("/robots", robotsRouter);
 
