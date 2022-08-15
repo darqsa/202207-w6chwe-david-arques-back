1a2c18ea5ab95bbc5653ad07f4b00f479515dfe1
Carlos <casantosmu@gmail.com>
2022-08-15 19:03:38 +0200

    Added toJSON whe moongose connects to ddbb

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,6 +6,18 @@ const debug = Debug("robots:database:index");
 
 const connectDB = (mongoUrl: string) =>
   new Promise((resolve, reject) => {
+    mongoose.set("toJSON", {
+      virtuals: true,
+      transform: (doc, ret) => {
+        const newDocument = { ...ret };
+        // eslint-disable-next-line no-underscore-dangle
+        delete newDocument.__v;
+        // eslint-disable-next-line no-underscore-dangle
+        delete newDocument._id;
+        return newDocument;
+      },
+    });
+
     mongoose.connect(mongoUrl, (error) => {
       if (error) {
         debug(chalk.red(`Error connecting to database. ${error.message}`));
